be0ebf691303a5d87e488be5ae448b5a06b858b8
dmitry.shapar <dmitry.shapar@altexsoft.com>
2018-02-05 13:58:31 +0200

    fixing extensions method to return datetime in UTC always

--- MarriageAgencyStatistics.Common/Extensions.cs
+++ MarriageAgencyStatistics.Common/Extensions.cs
@@ -12,8 +12,8 @@ namespace MarriageAgencyStatistics.Common
         public static string ToQueryString(this object obj)
         {
             var properties = from p in obj.GetType().GetProperties()
-                where p.GetValue(obj, null) != null
-                select p.Name + "=" + HttpUtility.UrlEncode(p.GetValue(obj, null).ToString());
+                             where p.GetValue(obj, null) != null
+                             select p.Name + "=" + HttpUtility.UrlEncode(p.GetValue(obj, null).ToString());
 
             return String.Join("&", properties.ToArray());
         }
@@ -25,7 +25,7 @@ namespace MarriageAgencyStatistics.Common
 
         public static DateTime GetFirstDayOfTheMonth(this DateTime date)
         {
-            return new DateTime(date.Year, date.Month, 1);
+            return new DateTime(date.Year, date.Month, 1, 0, 0, 0, DateTimeKind.Utc);
         }
 
         public static DateTime GetLastDayOfTheMonth(this DateTime date)
@@ -36,7 +36,7 @@ namespace MarriageAgencyStatistics.Common
 
         public static DateTime ToStartOfTheDay(this DateTime date)
         {
-            return new DateTime(date.Year, date.Month, date.Day);
+            return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, DateTimeKind.Utc);
         }
 
         public static DateTime ToEndOfTheDay(this DateTime date)
